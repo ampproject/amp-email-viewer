5a49a450e8da0f34d4adf793d9e1db8cc58a7562
Filip Stanis <fstanis@google.com>
2019-11-13 18:43:58 +0000

    use new render in example

--- example/frontend/index.js
+++ example/frontend/index.js
@@ -1,5 +1,17 @@
-import {Viewer} from '@ampproject/email-viewer/dist/viewer.mjs';
+import {FrameContainer} from '@ampproject/email-viewer/dist/viewer.mjs';
 
 const container = document.querySelector('#viewer');
-const viewer = new Viewer(container);
-viewer.render();
+const viewer = new FrameContainer(container, '/modules/bootstrap-page');
+viewer.render(`<!doctype html>
+<html ⚡4email allow-xhr-interception allow-viewer-render-template report-errors-to-viewer>
+<head>
+  <meta charset="utf-8">
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+  <script async src="https://cdn.ampproject.org/v0/amp-viewer-integration-0.1.js"></script>
+  <style amp4email-boilerplate>body{visibility:hidden}</style>
+</head>
+<body>
+  Hello, AMP4EMAIL world.
+</body>
+</html>
+`);
