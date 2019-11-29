3bf342fc072f605a36ef9c9d7b276e3743a6b472
Filip Stanis <fstanis@google.com>
2019-11-29 18:11:39 +0000

    add headtag module

--- src/preprocessing/preprocessing-modules/preprocessing-modules.md
+++ src/preprocessing/preprocessing-modules/preprocessing-modules.md
@@ -20,3 +20,45 @@ This module adds the following attributes to the AMP document's `<html>` tag:
 -   `report-errors-to-viewer`
 
 These are used to enable capabilities for the AMP viewer.
+
+## Head tag injections
+
+This module adds the following `<meta>` tags into `<head>`:
+
+-   `<meta name="amp-allowed-url-macros">` which determines which
+    [AMP variable substitutions](https://github.com/ampproject/amphtml/blob/master/spec/amp-var-substitutions.md)
+    are allowed. Currently, no AMP variable substitutions are supported in AMP
+    for Email.
+
+-   `<meta name="amp-action-whitelist">` which determines which
+    [AMP actions](https://amp.dev/documentation/guides-and-tutorials/learn/amp-actions-and-events/)
+    are allowed. Currently, the following actions are allowed in AMP for Email:
+    -   `*.show`
+    -   `*.hide`
+    -   `*.toggleVisibility`
+    -   `*.toggleClass`
+    -   `*.scrollTo`
+    -   `*.focus`
+    -   `AMP-CAROUSEL.goToSlide`
+    -   `AMP-IMAGE-LIGHTBOX.open`
+    -   `AMP-LIGHTBOX.open`
+    -   `AMP-LIGHTBOX.close`
+    -   `AMP-LIST.changeToLayoutContainer`
+    -   `AMP-LIST.refresh`
+    -   `AMP-SELECTOR.clear`
+    -   `AMP-SELECTOR.selectUp`
+    -   `AMP-SELECTOR.selectDown`
+    -   `AMP-SELECTOR.toggle`
+    -   `AMP-SIDEBAR.open`
+    -   `AMP-SIDEBAR.close`
+    -   `AMP-SIDEBAR.toggle`
+    -   `FORM.clear`
+    -   `FORM.submit`
+    -   `AMP.setState`
+
+In addition, it adds the following `<script>` tag to enable AMP viewer
+integration:
+
+```html
+<script async src="https://cdn.ampproject.org/v0/amp-viewer-integration-0.1.js">
+```
---
+++ src/test/preprocessing/preprocessing-modules/HeadTag.test.ts
@@ -0,0 +1,49 @@
+import { module as HeadTag } from '../../../preprocessing/preprocessing-modules/HeadTag';
+import { JSDOM } from 'jsdom';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      document: Document;
+      DOMParser: typeof DOMParser;
+    }
+  }
+}
+
+describe('HeadTag module', () => {
+  // tslint:disable:no-any
+  const config = {} as any;
+
+  beforeAll(() => {
+    const dom = new JSDOM();
+    global.document = dom.window.document;
+    global.DOMParser = dom.window.DOMParser;
+  });
+
+  afterAll(() => {
+    delete global.document;
+    delete global.DOMParser;
+  });
+
+  test('has correct name', () => {
+    expect(HeadTag.name).toBe('HeadTag');
+  });
+
+  test('injects into <head> tag', () => {
+    const code = `<!DOCTYPE html>
+<html amp4email>
+<head>
+<meta name="hello" content="hello,world">
+</head>
+<body>Hello, world!</body>
+</html>`;
+
+    const out = HeadTag.process(code, config);
+    expect(out).toBe(`<!DOCTYPE html>
+<html amp4email=""><head>
+<meta name="hello" content="hello,world">
+<meta name="amp-allowed-url-macros" content=""><meta name="amp-action-whitelist" content="*.show,*.hide,*.toggleVisibility,*.toggleClass,*.scrollTo,*.focus,AMP-CAROUSEL.goToSlide,AMP-IMAGE-LIGHTBOX.open,AMP-LIGHTBOX.open,AMP-LIGHTBOX.close,AMP-LIST.changeToLayoutContainer,AMP-LIST.refresh,AMP-SELECTOR.clear,AMP-SELECTOR.selectUp,AMP-SELECTOR.selectDown,AMP-SELECTOR.toggle,AMP-SIDEBAR.open,AMP-SIDEBAR.close,AMP-SIDEBAR.toggle,FORM.clear,FORM.submit,AMP.setState"><script src="https://cdn.ampproject.org/v0/amp-viewer-integration-0.1.js" async=""></script></head>
+<body>Hello, world!
+</body></html>`);
+  });
+});
