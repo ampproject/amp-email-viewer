9d9d8c56931ef16b288edfb0084d9907932a94cd
Filip Stanis <fstanis@google.com>
2019-11-29 18:51:48 +0000

    add hyperlink rewriter

--- src/preprocessing/preprocessing-modules/preprocessing-modules.md
+++ src/preprocessing/preprocessing-modules/preprocessing-modules.md
@@ -20,3 +20,14 @@ This module adds the following attributes to the AMP document's `<html>` tag:
 -   `report-errors-to-viewer`
 
 These are used to enable capabilities for the AMP viewer.
+
+## Hyperlink rewrite
+
+Rewrites all hyperlinks (`<a>` tags) to include the following attributes:
+
+-   `target="_blank"` to ensure the link opens in a new tab.
+-   `rel="noreferrer noopener"` to prevent setting the `Referer` HTTP header and
+    ensure new window can't access `window.opener`.
+
+If `linkRedirectURL` is set in the global config, the `href` attribute is
+rewritten to use the redirect URL instead of the original.
---
+++ src/test/preprocessing/preprocessing-modules/HyperlinkRewrite.test.ts
@@ -0,0 +1,69 @@
+import { module as HyperlinkRewrite } from '../../../preprocessing/preprocessing-modules/HyperlinkRewrite';
+import { JSDOM } from 'jsdom';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      DOMParser: typeof DOMParser;
+    }
+  }
+}
+
+describe('HyperlinkRewrite module', () => {
+  beforeAll(() => {
+    const dom = new JSDOM();
+    global.DOMParser = dom.window.DOMParser;
+  });
+
+  afterAll(() => {
+    delete global.DOMParser;
+  });
+
+  test('has correct name', () => {
+    expect(HyperlinkRewrite.name).toBe('HyperlinkRewrite');
+  });
+
+  test('adds hyperlink attributes', () => {
+    const code = `<!DOCTYPE html>
+<html amp4email>
+<head></head>
+<body>
+<a href="https://external.example/url">Hello</a>
+<a href="https://external.example/other" target="_top">Hello</a>
+</body>
+</html>`;
+
+    // tslint:disable:no-any
+    const out = HyperlinkRewrite.process(code, {} as any);
+    expect(out).toBe(`<!DOCTYPE html>
+<html amp4email=""><head></head>
+<body>
+<a href="https://external.example/url" target="_blank" rel="noreferrer noopener">Hello</a>
+<a href="https://external.example/other" target="_blank" rel="noreferrer noopener">Hello</a>
+
+</body></html>`);
+  });
+
+  test('adds hyperlink attributes and changes href', () => {
+    const code = `<!DOCTYPE html>
+<html amp4email>
+<head></head>
+<body>
+<a href="https://external.example/url">Hello</a>
+<a href="https://external.example/other" target="_top">Hello</a>
+</body>
+</html>`;
+
+    // tslint:disable:no-any
+    const out = HyperlinkRewrite.process(code, {
+      linkRedirectURL: 'https://redirect.example/goto?url=%s',
+    } as any);
+    expect(out).toBe(`<!DOCTYPE html>
+<html amp4email=""><head></head>
+<body>
+<a href="https://redirect.example/goto?url=https%3A%2F%2Fexternal.example%2Furl" target="_blank" rel="noreferrer noopener" title="https://external.example/url">Hello</a>
+<a href="https://redirect.example/goto?url=https%3A%2F%2Fexternal.example%2Fother" target="_blank" rel="noreferrer noopener" title="https://external.example/other">Hello</a>
+
+</body></html>`);
+  });
+});
