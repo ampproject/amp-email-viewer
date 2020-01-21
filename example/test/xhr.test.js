8ea330e4686f0ec7f5754aa4b6567399b1eb591a
Filip Stanis <fstanis@google.com>
2020-01-21 17:20:20 +0000

    add all preprocessing modules

--- example/test/xhr.test.js
+++ example/test/xhr.test.js
@@ -1,16 +1,17 @@
 const { loadAMP } = require('./util/loader');
 
 const AMP_LIST_CODE = `<!DOCTYPE html>
-<html ⚡4email allow-xhr-interception>
+<html ⚡4email>
 <head>
   <meta charset="utf-8">
   <script async src="https://cdn.ampproject.org/v0.js"></script>
-  <script async src="https://cdn.ampproject.org/v0/amp-viewer-integration-0.1.js"></script>
   <script async custom-element="amp-list" src="https://cdn.ampproject.org/v0/amp-list-0.1.js"></script>
+  <script async custom-template="amp-mustache" src="https://cdn.ampproject.org/v0/amp-mustache-0.2.js"></script>
   <style amp4email-boilerplate>body{visibility:hidden}</style>
 </head>
 <body>
   <amp-list layout="fixed-height" height="100" src="https://amp.dev/static/samples/json/cart.json">
+    <template type="amp-mustache"></template>
   </amp-list>
 </body>
 </html>
